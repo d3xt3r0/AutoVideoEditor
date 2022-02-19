56d9686a1840521f36f3c3f334f950d405621b9f
d3xt3ro <prathapss87@gmail.com>
2022-02-19 19:02:44 +0530

    Version 1 fully working

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from moviepy.editor import *
 def cut_video(loc, start, interval, clip_duration, output_name):
 
     stop = 0.0
+    
     try:
         clip = VideoFileClip(loc)
         duration = clip.duration
+        final = clip.subclip(start, start+clip_duration)
         while stop < duration-10:
             start += interval
             stop = start + clip_duration
@@ -23,9 +25,9 @@ if __name__ == "__main__":
     print("Welcome to AutoVideoEditor!")
 
     loc = input("> Enter input file path [eg: res/input.mp4]")
-    start = input("> Enter the start time [eg: 0.0]")
-    interval = input("> Enter the interval time [eg: 10 ie, every 10 seconds]")
-    clip_duration = input("> Enter the clip duration [eg: 2 ie, for 2 seconds]")
+    start = float(input("> Enter the start time [eg: 0.0]"))
+    interval = int(input("> Enter the interval time [eg: 10 ie, every 10 seconds]"))
+    clip_duration = int(input("> Enter the clip duration [eg: 2 ie, for 2 seconds]"))
     output = input("> Enter the output path [eg: res/output/output.mp4]")
 
     cut_video(loc, start, interval, clip_duration, output)
