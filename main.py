ddd1b3360189f520700d37e51a63f68e92395e90
d3xt3ro <prathapss87@gmail.com>
2022-02-19 18:53:39 +0530

    Modify cut function

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from moviepy.editor import *
 
-def cut_video(loc, start, stop, output_name):
+def cut_video(loc, start, interval, clip_duration, output_name):
+
+    stop = 0.0
     try:
-        ffmpeg_extract_subclip(loc, start, stop, targetname=output_name)
+        clip = VideoFileClip(loc)
+        duration = clip.duration
+        while stop < duration-10:
+            start += interval
+            stop = start + clip_duration
+            print("#")
+            tempclip = clip.subclip(start, stop)
+            final = concatenate_videoclips([final, tempclip])
+        final.write_videofile(output_name)
         return True
+
     except FileNotFoundError:
         return False
 
 if __name__ == "__main__":
     print("Welcome to AutoVideoEditor!")
 
-    cut_video('/res/test.mkv', 0.0, 0.5, 'test.mp4')
+    loc = input("> Enter input file path [eg: res/input.mp4]")
+    start = input("> Enter the start time [eg: 0.0]")
+    interval = input("> Enter the interval time [eg: 10 ie, every 10 seconds]")
+    clip_duration = input("> Enter the clip duration [eg: 2 ie, for 2 seconds]")
+    output = input("> Enter the output path [eg: res/output/output.mp4]")
+
+    cut_video(loc, start, interval, clip_duration, output)
+
+    
+
+        
+
+
+    
     
     
